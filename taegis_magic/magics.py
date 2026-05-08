175e7a5431a8a572f16019829949a62bec4d969d
chris.langreo <chris.langreo@sophos.com>
2026-05-08 11:45:16 -0400

    add print of cache digest
    Signed-off-by: chris.langreo <chris.langreo@sophos.com>

--- taegis_magic/magics.py
+++ taegis_magic/magics.py
@@ -258,6 +258,7 @@ class TaegisMagics(Magics):
                     cell = ""
 
                 cache_digest = hashlib.sha256(bytes(line + cell, "utf-8")).hexdigest()
+                print(f"This is v2026.05.07. Cache digest is {cache_digest}")
                 cache = get_cache_item(notebook_fp, magic_args.assign, cache_digest)
                 if cache:
                     log.info(f"{magic_args.assign} found in cache...")
